1748f61297fe60ef259751fb444b94a7e81d2831
Ekaterina1994 <katishteno@mail.ru>
2022-09-19 13:38:17 +0300

    #6-create services/add quiz service methods

--- client/src/app/service/serviceStub/QuizServiceStub.ts
+++ client/src/app/service/serviceStub/QuizServiceStub.ts
@@ -1,11 +1,12 @@
 import {QuizRoundModel} from "src/app/model/QuizRoundModel";
 import questionsDTO from "src/app/questionsRoundDTO.json";
 import {questionsRoundDTO} from "src/app/model/QuestionsRoundDTO";
+import {QuizServiceMethods} from "src/app/service/QuizServiceMethods";
 
 /**
  * Use when the client doesn't have internet
  */
-export class QuizServiceStub {
+export class QuizServiceStub implements QuizServiceMethods {
 
   /**
    * Return array with questions, answers, right answers, keys
