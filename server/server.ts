00503e089fd2db399bb601f02c98a64153ac91d3
Ekaterina1994 <katishteno@mail.ru>
2022-10-13 20:54:29 +0300

    add eslint, ts  and webpack config

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,15 @@
-import express from "express";
-import quiz from "../server/quiz.json";
+const express = require("express");
+const quiz = require("./quiz.json");
 
 const app = express();
 
 const PORT = 5000;
 
-app.get("/api/quiz", (req, res) =>
-{
+app.get("/api/quiz", (req, res) => {
   res.json(quiz);
 });
 
-app.listen(PORT, (req, res) =>
-{
+app.listen(PORT, (req, res) => {
   console.log(`Server started on PORT ${PORT}`);
 });
 
