92cd39459ac14099e8f1da60b117858959003a83
Ekaterina1994 <katishteno@mail.ru>
2022-10-31 18:30:27 +0300

    #12-create-server-new/change scripts

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,40 +1,83 @@
 import path from "path";
-import nodeExternals from "webpack-node-externals";
-import WebpackShellPlugin from "webpack-shell-plugin";
+import { fileURLToPath } from 'url';
+console.log('webpack works!');
+// import nodeExternals from "webpack-node-externals";
+// import WebpackShellPlugin from "webpack-shell-plugin";
+// import UglifyJsPlugin from "uglifyjs-webpack-plugin";
 
-let mode = "development";
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
-}
+// const BUILD_DIRECTORY = path.dirname('./build');
 
-module.exports = {
-  entry: "server.ts",
-  mode,
-  target: "node",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "server.js",
-    clean: true,
+// let NODE_ENV = "development";
+// if (process.env.NODE_ENV === "production") {
+//   NODE_ENV = "production";
+// }
+
+// export default {
+//   entry: "./server.ts",
+//   mode: NODE_ENV,
+//   target: "node",
+//   output: {
+//     path: BUILD_DIRECTORY,
+//     filename: "./server.js",
+//     clean: true,
+//   },
+//   resolve: {
+//     extensions: [".ts", ".js"],
+//   },
+//   devtool: "source-map",
+//   devServer: {
+//     hot: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts$/,
+//         use: ["ts-loader"],
+//       },
+//     ],
+//   },
+//   // optimization: {
+//   //   // minimizer: [new UglifyJsPlugin({
+//   //   //   // sourceMap: true,
+//   //   //   // compress: {
+//   //   //   //   warnings: false,
+//   //   //   // }
+//   //   // })],
+//   // },
+//   externals: [nodeExternals()],
+//   watch: NODE_ENV,
+//   plugins: [
+//     new WebpackShellPlugin({
+//       onBuildStart: ["pnpm run start"],
+//     }),
+//   ],
+// };
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+
+export default {
+  mode: 'development',
+  entry: {
+      'index': './index.ts',
+  },
+  target: 'node',
+  module: {
+      rules: [
+          {
+              test: /\.tsx?$/,
+              use: 'ts-loader',
+              exclude: /node_modules/
+          }
+      ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
+      extensions: [ '.tsx', '.ts', '.js' ]
   },
-  devtool: "source-map",
-
-  devServer: {
-    hot: true,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: ["ts-loader"],
-      },
-    ],
-  },
-  externals: [nodeExternals()],
-  watch: NODE_ENV === "development",
-  plugins: [
-  ]
+  output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, './js'),
+  }
 };
