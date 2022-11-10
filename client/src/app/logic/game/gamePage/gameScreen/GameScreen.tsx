0a5197362c4f4d5678dea241be7ae9f0b971de5c
skif-cezar <skif.cezar@gmail.com>
2022-11-10 22:22:01 +0300

    #38-add-second-screen-to-game-part/add static circles

--- client/src/app/logic/game/gamePage/gameScreen/GameScreen.tsx
+++ client/src/app/logic/game/gamePage/gameScreen/GameScreen.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, {useContext} from "react";
 import clsx from "clsx";
 import styles from "src/app/logic/game/gamePage/GamePage.module.scss";
+import {GameContext, GameStoreInterface} from "src/app/logic/game/GameStore";
+import {getElementByIndex} from "src/app/utils/arrayUtils";
+import {RoundDTO} from "src/app/model/RoundDTO";
 
 /**
  * Main game screen
  */
 export const GameScreen: React.FC = () => {
   const GAME_MAIN_STYLES = clsx(styles.main);
+  const GAME_CIRCLES_STYLES = clsx(styles.circles);
+  const GAME_CIRCLE_STYLES = clsx(styles.circle);
+  const {rounds}: GameStoreInterface = useContext(GameContext);
 
   return (
     <div className={GAME_MAIN_STYLES}>
-      <h1>Main game screen</h1>
+      <div className={GAME_CIRCLES_STYLES}>
+        {rounds
+          .map((round: RoundDTO) => {
+            return (
+              <div
+                className={GAME_CIRCLE_STYLES}
+                key={round.id}
+              >
+                {parseInt(getElementByIndex(rounds, parseInt(round.id, 10)).id, 10) + 1}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
